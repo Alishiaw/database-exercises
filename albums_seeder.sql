8f758e57284ec57e6bc8ab126aab4c1a543803be
AlishiaWilcher <stown400@gmail.com>
2017-12-11 09:58:46 -0600

    completed the Select Basic Statements

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -2,33 +2,58 @@ USE codeup_test_db;
 
 INSERT INTO albums (artist, name, release_date, sales, genre)
 
-VALUES ('Michael Jackson', 'Thriller', 1982, 47.3, 'Pop, Rock, R&B');
+VALUES
+  ('Michael Jackson', 'Thriller', 1982, 47.3, 'Pop, Rock, R&B'),
+  ('AC/DC', 'Back in Black', 1980, 26.1,'Hard rock'),
+  ('Pink Floyd','The Dark Side of the Moon',1973, 24.2,'Progressive rock'),
+  ('Meat Loaf','Bat Out of Hell',1977,21.5,'Hard rock, Progressive rock'),
+  ('Whitney Houston','The Bodyguard',1992,28.4,'R&B, Soul, Pop'),
+  ('Eagles','Their Greatest Hits',1976,32.2,'Rock, Soft rock, Folk rock'),
+  ('Bee Gees','Saturday Night Fever',1977,20.6,'Disco'),
+  ('Fleetwood Mac','Rumours',1977,27.9,'Soft rock'),
+  ('Shania Twain','Come On Over',1997,29.6,'Country, Pop'),
+  ('Led Zeppelin','Led Zeppelin IV',1971,29,'Hard rock, heavy metal'),
+  ('Michael Jackson','Bad',1987,20.3,'Pop, funk, rock'),
+  ('Alanis Morissette','Jagged Little Pill',1995,24.8,'Alternative rock'),
+  ('Celine Dion','Falling into You',1996,20.2,'Pop, Soft rock'),
+  ('The Beatles','Sgt. Pepper''s Lonely Hearts Club Band',1967,13.1,'Rock'),
+  ('Eagles','Hotel California',1976,21.5,'Rock, soft rock, folk rock'),
+  ('Mariah Carey','Music Box',1993,19,'Pop, R&B, Rock'),
+  ('Michael Jackson','Dangerous',1991,17.6,'Rock, Funk, Pop'),
+  ('Various artists','Dirty Dancing',1987,17.9,'Pop, rock, R&B'),
+  ('Celine Dion','Let''s Talk About Love',1997,19.3,'Pop, Soft rock'),
+  ('The Beatles','1',2000,21.6,'Rock'),
+  ('Adele','21',2011,22.3,'Pop, soul'),
+  ('The Beatles','Abbey Road',1969,14.4,'Rock'),
+  ('Bruce Springsteen','Born in the U.S.A.',1984,19.6,'Rock'),
+  ('Dire Straits','Brothers in Arms',1985,17.7,'Rock'),
+  ('James Horner','Titanic: Music from the Motion Picture',1997,18.1,'Soundtrack'),
+  ('Madonna','The Immaculate Collection',1990,19.4,'Pop, Dance'),
+  ('Metallica','Metallica',1991,19.9,'Thrash metal, heavy metal'),
+  ('Nirvana','Nevermind',1991,16.7,'Grunge, alternative rock'),
+  ('Pink Floyd','The Wall',1979,17.6,'Progressive rock'),
+  ('Santana','Supernatural',1999,20.5,'Rock'),
+  ('Guns N'' Roses','Appetite for Destruction',1987,21.3,'Heavy metal, hard rock');
 
-INSERT INTO albums (artist, name, release_date, sales, genre)
-
-VALUES ('AC/DC', 'Back in Black', 1980, 26.1,'Hard rock');
 
 
 
 
 
-#  [show]
-# 50	[23]
-# Pink Floyd	The Dark Side of the Moon	1973	Progressive rock
-# 24.2 [show]
-# 45	[26][27]
-# Meat Loaf	Bat Out of Hell	1977	Hard rock, Progressive rock
-# 21.5 [show]
-# 43	[28]
-# Whitney Houston / Various artists	The Bodyguard	1992	R&B, Soul, Pop, Soundtrack
-# 28.4 [show]
-# 42	[33][34]
-# Eagles	Their Greatest Hits (1971–1975)	1976	Rock, Soft rock, Folk rock
-# 32.2 [show]
-# 42	[35][36]
-# Bee Gees / Various artists	Saturday Night Fever	1977	Disco
-# 20.6 [show]
-# 40	[38][39]
-# Fleetwood Mac	Rumours	1977	Soft rock
-# 27.9 [show]
-# 40	[42][43]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ select_exercise.sql
@@ -0,0 +1,13 @@
+use codeup_test_db;
+
+select name from albums where artist = 'Pink Floyd';
+
+select release_date from albums where name ='Sgt.Pepper\' Lonely Hearts Club Band';
+
+select genre from albums where name ='Nevermind';
+
+select * from albums where release_date BETWEEN 1990 AND 1999;
+
+select * from albums where sale < '20';
+
+select * from albums where genre = 'Rock';
